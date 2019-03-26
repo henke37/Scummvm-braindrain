6b41bf837977f1035b0d121d518e4ffb622965af
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-03-26 17:51:16 +0100

    Beginnings of RTTI reading.

---
+++ DebugHelp/RTTI/CompleteObjectLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebugHelp.RTTI {
+	internal class CompleteObjectLocator {
+		public uint signature;
+		public uint objectRootOffset;
+		public uint classDescriptorOffset;
+	}
+}
---
+++ DebugHelp/RTTI/RTTIReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebugHelp.RTTI {
+	public class RTTIReader {
+		private IProcessMemoryReader processMemoryReader;
+
+		private Dictionary<uint, CompleteObjectLocator> completeObjectLocatorMap;
+
+		public RTTIReader(IProcessMemoryReader processMemoryReader) {
+			this.processMemoryReader = processMemoryReader ?? throw new ArgumentNullException(nameof(processMemoryReader));
+
+			completeObjectLocatorMap = new Dictionary<uint, CompleteObjectLocator>();
+		}
+
+		private CompleteObjectLocator readObjPtr(uint objAddr) {
+			var vtblPtrVal = processMemoryReader.ReadUInt32At(objAddr);
+			var metaPtrVal = processMemoryReader.ReadUInt32At(vtblPtrVal - 4);
+
+			if(completeObjectLocatorMap.TryGetValue(metaPtrVal, out CompleteObjectLocator objectLocator)) {
+				return objectLocator;
+			}
+
+			throw new NotImplementedException();
+		}
+
+		public string GetMangledClassNameFromObjPtr(uint objAddr) {
+			readObjPtr(objAddr);
+			throw new NotImplementedException();
+		}
+
+		public bool IsObjectInheritingMangledType(uint objAddr, string mangledName) {
+			readObjPtr(objAddr);
+			throw new NotImplementedException();
+		}
+	}
+}
