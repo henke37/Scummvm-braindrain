b754aaa28b01cce91279de2e7397092b667d0cdf
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-03-28 22:54:41 +0100

    Autoupdate the test form

--- TestProj/TestForm.Designer.cs
+++ TestProj/TestForm.Designer.cs
@@ -23,20 +23,42 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+			this.statusTxt = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// UpdateTimer
+			// 
+			this.UpdateTimer.Enabled = true;
+			this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+			// 
+			// statusTxt
+			// 
+			this.statusTxt.AutoSize = true;
+			this.statusTxt.Location = new System.Drawing.Point(13, 13);
+			this.statusTxt.Name = "statusTxt";
+			this.statusTxt.Size = new System.Drawing.Size(35, 13);
+			this.statusTxt.TabIndex = 0;
+			this.statusTxt.Text = "label1";
 			// 
 			// TestForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.statusTxt);
 			this.Name = "TestForm";
 			this.Text = "TestForm";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Timer UpdateTimer;
+		private System.Windows.Forms.Label statusTxt;
 	}
 }
 
--- TestProj/TestForm.cs
+++ TestProj/TestForm.cs
@@ -2,6 +2,7 @@
 using DrainLib.Engines;
 using System;
 using System.Windows.Forms;
+using TestProj.Properties;
 
 namespace TestProj {
 	public partial class TestForm : Form {
@@ -17,6 +18,19 @@ namespace TestProj {
 		}
 
 		private new void Update() {
+			UpdateConnector();
+			if(!connector.Connected) {
+				statusTxt.Text = Resources.Status_NotConnected;
+				return;
+			}
+			if(engine==null) {
+				statusTxt.Text = Resources.Status_NoGame;
+				return;
+			}
+			statusTxt.Text = engine.ToString();
+		}
+
+		private void UpdateConnector() {
 			if(!connector.Connected) {
 				try {
 					connector.Connect();
@@ -29,6 +43,10 @@ namespace TestProj {
 				engine = connector.GetEngine();
 			}
 			if(engine == null) return;
+		}
+
+		private void UpdateTimer_Tick(object sender, EventArgs e) {
+			Update();
 		}
 	}
 }
