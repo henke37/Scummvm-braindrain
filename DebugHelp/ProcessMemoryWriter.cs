a8d2486ab157963232b8f9f42637df6130fd9e42
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-04-08 16:25:17 +0200

    Further design tweaks to the skeleton.

--- DebugHelp/ProcessMemoryWriter.cs
+++ DebugHelp/ProcessMemoryWriter.cs
@@ -6,31 +6,32 @@ using System.Security;
 namespace DebugHelp {
 	public abstract class ProcessMemoryWriter {
 
-		public byte[] WriteBytes(uint addr, uint size) {
-			var buff = new Byte[size];
-			WriteBytes(addr, size, buff);
-			return buff;
+		[SecuritySafeCritical]
+		public abstract void WriteBytes(byte[] srcBuff, uint dstAddr, uint size);
+
+		[SecurityCritical]
+		public virtual unsafe void WriteBytes(void* srcBuff, uint dstAddr, uint size) {
+			Byte[] buffArr = GetWriteBuff(size);
+			Marshal.Copy((IntPtr)srcBuff, buffArr, 0, (int)size);
+			WriteBytes(buffArr, dstAddr, size);
+		}
+
+		public void WriteByte(byte value, uint dstAddr) {
+			scratchBuff[0] = value;
+			WriteBytes(scratchBuff, dstAddr, 1);
 		}
 
 		[SecuritySafeCritical]
-		public abstract void WriteBytes(uint addr, uint size, byte[] buff);
-
-		[SecurityCritical]
-		public virtual unsafe void WriteBytes(uint addr, uint size, void* buff) {
-			Byte[] buffArr = new byte[size];
-			Marshal.Copy((IntPtr)buff, buffArr, 0, (int)size);
-			WriteBytes(addr, size, buffArr);
-		}
-
-		public Byte WriteByte(uint addr) {
-			WriteBytes(addr, 1, scratchBuff);
-			return scratchBuff[0];
+		public unsafe void WriteStruct<T>(uint dstAddr, ref T buff) where T : unmanaged {
+			fixed (void* buffP = &buff) {
+				WriteBytes(buffP, dstAddr, (uint)sizeof(T));
+			}
 		}
 
 
 		protected Byte[] scratchBuff = new Byte[16];
 
-		protected byte[] GetReadBuff(uint count) {
+		protected byte[] GetWriteBuff(uint count) {
 			return count <= scratchBuff.Length ? scratchBuff : new byte[count];
 		}
 	}
