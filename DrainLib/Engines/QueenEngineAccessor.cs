08af7b63c7c580d81be5faefe821aa8f4b748fcd
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-04-13 23:57:58 +0200

    Read the inventory.

--- DrainLib/Engines/QueenEngineAccessor.cs
+++ DrainLib/Engines/QueenEngineAccessor.cs
@@ -8,6 +8,8 @@ namespace DrainLib.Engines {
 		private uint currentRoomOffset;
 		private uint gameStateOffset;
 		private const uint gameStateCount = 211;
+		private uint numItemsOffset;
+		private uint itemDataOffset;
 
 		internal QueenEngineAccessor(ScummVMConnector connector, uint engineAddr) : base(connector, engineAddr) {
 		}
@@ -21,6 +23,8 @@ namespace DrainLib.Engines {
 			var logicClSymb = Connector.resolver.FindClass("Queen::Logic");
 			currentRoomOffset = Connector.resolver.FieldOffset(logicClSymb, "_currentRoom");
 			gameStateOffset = Connector.resolver.FieldOffset(logicClSymb, "_gameState");
+			numItemsOffset = Connector.resolver.FieldOffset(logicClSymb, "_numItems");
+			itemDataOffset = Connector.resolver.FieldOffset(logicClSymb, "_itemData");
 		}
 
 		public QueenState GetState() {
@@ -46,7 +50,16 @@ namespace DrainLib.Engines {
 			state.CurrentRoom = Connector.memoryReader.ReadUInt16(logicPtrVal + currentRoomOffset);
 			state.GameState = Connector.memoryReader.ReadInt16Array(logicPtrVal + gameStateOffset, gameStateCount);
 
+			state.Inventory = ReadInventory(logicPtrVal);
+
 			return state;
+		}
+
+		private QueenState.ItemData[] ReadInventory(uint logicPtrVal) {
+			var numItems = Connector.memoryReader.ReadUInt16(logicPtrVal + numItemsOffset);
+			var itemDataPtrVal = Connector.memoryReader.ReadUInt32(logicPtrVal + itemDataOffset);
+
+			return Connector.memoryReader.ReadStructArr<QueenState.ItemData>(itemDataPtrVal, numItems);
 		}
 	}
 
@@ -54,6 +67,43 @@ namespace DrainLib.Engines {
 		public uint CurrentRoom;
 		public short[] GameState;
 		public LogicMode LogicMode;
+		public ItemData[] Inventory;
+
+
+		public struct ItemData {
+			public short NameId;
+			public ushort DescId;
+			public ItemState State;
+			public ushort Frame;
+			public short SfxDesc;
+		}
+
+		[Flags]
+		public enum ItemState : ushort {
+			GrabNone = 0,
+			GrabDown = 1,
+			GrabUp = 2,
+			GrabMid = 3,
+
+			DirBack = 0,
+			DirRight = 1 << 2,
+			DirLeft = 2 << 2,
+			DirFront = 3 << 2,
+
+			VerbNone = 0,
+			VerbOpen = 1 << 4,
+			VerbClose = 3 << 4,
+			VerbLookAt = 6 << 4,
+			VerbMove = 7 << 4,
+			VerbGive = 8 << 4,
+			VerbTalkTo = 9 << 4,
+			VerbUse = 12 << 4,
+			VerbPickUp = 14 << 4,
+
+			On = 1 << 8,
+			Talk = 1 << 9,
+			Use = 1 << 10
+		}
 	}
 
 	public enum LogicMode {
