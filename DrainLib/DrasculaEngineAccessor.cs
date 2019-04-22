7d018a075dc4598aab15b21eefb84930ba0e1af1
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-04-22 03:38:52 +0200

    Basic drascula state

--- DrainLib/DrasculaEngineAccessor.cs
+++ DrainLib/DrasculaEngineAccessor.cs
@@ -2,7 +2,14 @@
 
 namespace DrainLib {
 	public class DrasculaEngineAccessor : ADBaseEngineAccessor {
+		#region Symbol data
 		private uint roomNumberOffset;
+		private uint flagsOffset;
+		private const uint NumFlags = 50;
+		private uint currentChapterOffset;
+		private uint inventoryObjectsOffset;
+		private const uint InventorySize = 43;
+		#endregion
 
 		public DrasculaEngineAccessor(ScummVMConnector connector, uint engineAddr) : base(connector, engineAddr) {
 		}
@@ -11,6 +18,9 @@ namespace DrainLib {
 			var engineClSymb = Connector.resolver.FindClass("Drascula::DrasculaEngine");
 			var descOffset = Connector.resolver.FieldOffset(engineClSymb, "_gameDescription");
 			roomNumberOffset = Connector.resolver.FieldOffset(engineClSymb, "_roomNumber");
+			flagsOffset = Connector.resolver.FieldOffset(engineClSymb, "flags");
+			currentChapterOffset = Connector.resolver.FieldOffset(engineClSymb, "currentChapter");
+			inventoryObjectsOffset = Connector.resolver.FieldOffset(engineClSymb, "inventoryObjects");
 
 			LoadADSymbols(descOffset, true);
 		}
@@ -18,11 +28,17 @@ namespace DrainLib {
 		public DrasculaState GetState() {
 			var state = new DrasculaState();
 			state.RoomNumber = Connector.memoryReader.ReadInt32(EngineAddr + roomNumberOffset);
+			state.Flags = Connector.memoryReader.ReadInt32Array(EngineAddr + flagsOffset, NumFlags);
+			state.CurrentChapter = Connector.memoryReader.ReadInt32(EngineAddr + currentChapterOffset);
+			state.Inventory = Connector.memoryReader.ReadInt32Array(EngineAddr + inventoryObjectsOffset, InventorySize);
 			return state;
 		}
 	}
 
 	public class DrasculaState {
 		public int RoomNumber;
+		public int[] Flags;
+		public int CurrentChapter;
+		public int[] Inventory;
 	}
 }
