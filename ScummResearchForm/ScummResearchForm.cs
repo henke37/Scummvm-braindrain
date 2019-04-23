1397c34899dc6899c3da2e9c12dd34433a5edb2f
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-04-23 19:36:35 +0200

    Basic skeleton for a research form

---
+++ ScummResearchForm/ScummResearchForm.cs
@@ -0,0 +1,56 @@
+﻿using DebugHelp;
+using DrainLib;
+using DrainLib.Engines;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScummResearchForm {
+	public partial class ScummResearchForm : Form {
+		private ScummVMConnector connector;
+		private BaseEngineAccessor engine;
+
+		public ScummResearchForm() {
+			InitializeComponent();
+
+			connector = new ScummVMConnector();
+			Update();
+		}
+
+		private void UpdateConnector() {
+			if(!connector.Connected) {
+				try {
+					connector.Connect();
+				} catch(ProcessNotFoundException) {
+					return;
+				} catch(IncompleteReadException) {
+					return;
+				}
+			}
+
+			if(!engine.IsActiveEngine) engine = null;
+			if(engine == null) {
+				engine = connector.GetEngine();
+			}
+			if(engine == null) return;
+		}
+
+		private new void Update() {
+			UpdateConnector();
+		}
+
+		private void UpdateTimer_Tick(object sender, EventArgs e) {
+			//turn off the timer while processing to prevent multiple processings run at the same time
+			//also makes exceptions not turn it back on
+			UpdateTimer.Enabled = false;
+			Update();
+			UpdateTimer.Enabled = true;
+		}
+	}
+}
