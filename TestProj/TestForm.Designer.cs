10803ca46c8f0872b6073b8cc2e23b8029946f0a
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-22 20:11:33 +0200

    Resize the form to not have so much wasted space.

--- TestProj/TestForm.Designer.cs
+++ TestProj/TestForm.Designer.cs
@@ -46,7 +46,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(206, 152);
 			this.Controls.Add(this.statusTxt);
 			this.Name = "TestForm";
 			this.Text = "TestForm";
