96cd83e570b4e4e1e1f1714be703cffc83d2f36f
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-03-04 20:41:02 +0100

    Video decoder for private-eye

--- DrainLib/Engines/PrivateEngineAccessor.cs
+++ DrainLib/Engines/PrivateEngineAccessor.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace DrainLib.Engines {
 	public class PrivateEngineAccessor : ADBaseEngineAccessor {
+
+		#region symbol data
+		private int videoDecoderOffset;
+		#endregion
+
+		private VideoAccessor videoAccessor;
+
 		public PrivateEngineAccessor(ScummVMConnector connector, IntPtr engineAddr) : base(connector, engineAddr) {
+			videoAccessor = new VideoAccessor(Connector);
 		}
 
 		internal override void LoadSymbols() {
 			var engineCl = Resolver.FindClass("Private::PrivateEngine");
-			var descOffset=Resolver.FieldOffset(engineCl, "_gameDescription");
+			var descOffset = Resolver.FieldOffset(engineCl, "_gameDescription");
+			videoDecoderOffset = Resolver.FieldOffset(engineCl, "_videoDecoder");
 
 			LoadADSymbols(descOffset, true);
+		}
+
+		public override VideoState? GetVideoState() {
+			var decoderAddr = MemoryReader.ReadIntPtr(EngineAddr + videoDecoderOffset);
+			if(decoderAddr==IntPtr.Zero) {
+				return null;
+			}
+			return videoAccessor.ReadDecoder(decoderAddr);
 		}
 	}
 }
