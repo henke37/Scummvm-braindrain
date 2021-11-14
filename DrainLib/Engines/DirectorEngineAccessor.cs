49fd55e65fec9c5dee4ce49127d848b03d447c49
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-11-14 13:01:43 +0100

    Beginnings of director support

--- DrainLib/Engines/DirectorEngineAccessor.cs
+++ DrainLib/Engines/DirectorEngineAccessor.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace DrainLib.Engines {
 	class DirectorEngineAccessor : ADBaseEngineAccessor {
+		#region Symbol data
+		//engine class
+		private int lingoOffset;
+		private int stageOffset;
+		#endregion
+
+		#region Semistatic data
+		private IntPtr lingoAddr;
+		private IntPtr stageAddr;
+		#endregion
 		public DirectorEngineAccessor(ScummVMConnector connector, IntPtr engineAddr) : base(connector, engineAddr) {
+			LoadSemiStaticData();
+		}
+
+
+		private void LoadSemiStaticData() {
+			stageAddr = MemoryReader.ReadIntPtr(EngineAddr + stageOffset);
+			lingoAddr = MemoryReader.ReadIntPtr(EngineAddr + lingoOffset);
 		}
 
 		internal override void LoadSymbols() {
 			var engineCl = Resolver.FindClass("Director::DirectorEngine");
 			var descriptorOffset = Resolver.FieldOffset(engineCl, "_gameDescription");
+			stageOffset = Resolver.FieldOffset(engineCl, "_stage");
+			lingoOffset = Resolver.FieldOffset(engineCl, "_lingo");
 
 			LoadADSymbols(descriptorOffset, true);
 		}
